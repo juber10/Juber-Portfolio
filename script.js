e9eb82cca254b51803d7e2344d5e24ed9c4cd710
juber10 <ansari2002juber@gmail.com>
2025-09-02 22:02:07 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    links.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
+
+
+
+    const skillBars = document.querySelectorAll('.skill-bar-inner');
+
+    window.addEventListener('scroll', () => {
+      skillBars.forEach(bar => {
+        const sectionTop = bar.parentElement.parentElement.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if (sectionTop < windowHeight - 50) {
+          bar.style.width = bar.getAttribute('data-skill');
+        }
+      });
+    });
+
+    // Get current year dynamically
+const year = new Date().getFullYear();
+
+// Add copyright text
+document.getElementById("copyright").innerHTML = 
+  `&copy; ${year} Mohd Juber. All Rights Reserved.`;
+
+  
